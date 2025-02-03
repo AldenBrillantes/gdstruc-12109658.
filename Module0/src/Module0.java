83569c64047214b7ae40abeaf2934fbc41897035
Alden Brillantes <aldenkarlyle.brillantes@benilde.edu.ph>
2025-02-03 08:59:07 +0300

    Quiz 1 done with no bugs

---
+++ Module0/src/Module0.java
@@ -0,0 +1,23 @@
+public class Module0 {
+public static void main(String[]args) {
+int[] nums = new int[5];
+
+    nums[0] = 14;
+    nums[1] = -5;
+    nums[2] = 23;
+    nums[3] = 19;
+    nums[4] = 1;
+
+    int value = 19;
+    int index = -1;
+
+    for(int i = 0; i < nums.length; i++){
+        if(nums[i] == value){
+            index = i;
+            break;
+
+        }
+    }
+System.out.println(Stringvalue.value)
+}
+    }
